669d573aebce0f8dd302194f81dae06c899d8718
Ahaan12345 <88834585+Ahaan12345@users.noreply.github.com>
2022-05-10 13:44:26 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,5 @@
+function login(){
+    player1_name=document.getElementById("player1input").innerHTML;
+    player2_name=document.getElementById("player2input").innerHTML;
+    window.location="quiz_game_page.html";
+}
